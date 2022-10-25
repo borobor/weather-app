7730c4dfb3ced51d794c935f866bc314a8740f26
BornaLap <bornajurisa@gmailcom>
2022-10-25 18:56:22 +0200

    Display data on a card and add temp unit conversion

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ async function fetchData() {
   const response = await API.getInfo();
   const data = await response.json()
   //use object destructuring to only get important data
-  console.log(data);
   const { name, main, weather } = data;
   return { name, main, weather };
 }
@@ -16,9 +15,51 @@ async function fetchData() {
 fetchData()
   .then(data => {
     const city = data.name;
-    const tempF = data.main.temp;
+    const tempK = data.main.temp;
     const pressure = data.main.pressure;
     const forecast = data.weather[0].main;
-  
-    const weatherData = {city, tempF, pressure, forecast}
+
+    const tempC = (Math.round((tempK - 273) * 10) / 10);
+
+    const weatherData = { city, tempC, pressure, forecast }
+    showData(weatherData);
   })
+
+function showData(weatherData) {
+  const weatherWidget = document.querySelector('.widget');
+  const children = [...weatherWidget.children];
+  children.forEach(child => {
+    const divData = child.dataset.info;
+    if (divData === 'tempC') {
+      child.textContent = `${weatherData[divData]} °C`;
+      return
+    }
+    if (divData === 'pressure') {
+      child.textContent = `${weatherData[divData]} hPa`;
+      return
+    }
+
+    child.textContent = weatherData[divData];
+  })
+}
+
+const unitBtn = document.querySelector('.temp-unit');
+unitBtn.addEventListener('click', ({ target }) => {
+  const divTemp = document.querySelector(".widget-temp");
+  const currentUnit = divTemp.dataset.info;
+  if (currentUnit === 'tempC') {
+    let temp = divTemp.textContent.split(' ')[0];
+    let tempF = temp*(9/5) + 32;
+    divTemp.textContent = `${tempF} °F`;
+    divTemp.dataset.info = 'tempF';
+  }
+  if (currentUnit === 'tempF') {
+    let temp = divTemp.textContent.split(' ')[0];
+    let tempC = (temp-32)*5/9;
+    divTemp.textContent = `${tempC} °C`;
+    divTemp.dataset.info = 'tempC';
+  }
+})
+
+const refreshBtn = document.querySelector('.refresh');
+refreshBtn.addEventListener('click', fetchData);
